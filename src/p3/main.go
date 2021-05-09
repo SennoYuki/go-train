595799358bbe5da51178f994fdae3a4b4a2c82a8
SennoYuki <657372386@qq.com>
2021-05-09 22:43:55 +0800

    commit p3

--- src/p3/main.go
+++ src/p3/main.go
@@ -12,7 +12,24 @@ import (
 )
 
 func main() {
-	errgroup.WithContext(context.Background())
+	o := NewOption()
+	o.WithTimeout(time.Second * 10)
+	o.AddServer(new(EmptyServer), new(EmptyServer))
+	o.ListenSignal(syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	if err := o.Serve(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+type EmptyServer struct {
+}
+
+func (s EmptyServer) Run() error {
+	return nil
+}
+
+func (s EmptyServer) Stop() error {
+	return nil
 }
 
 type Server interface {
@@ -21,9 +38,9 @@ type Server interface {
 }
 
 type Option struct {
+	ss      []Server
+	signals []os.Signal
 	timeout time.Duration
-	ss      []*Server
-	extra   []func() error
 }
 
 func NewOption() *Option {
@@ -34,35 +51,42 @@ func (o *Option) WithTimeout(duration time.Duration) {
 	o.timeout = duration
 }
 
-func (o *Option) AddDefaultSignalHandler() {
-	o.extra = append(o.extra, defaultSignalHandle)
+func (o *Option) ListenSignal(signals ...os.Signal) {
+	o.signals = signals
+}
+
+func (o *Option) AddServer(servers ...Server) {
+	o.ss = append(o.ss, servers...)
 }
 
 func (o *Option) Serve() error {
 	g, ctx := errgroup.WithContext(context.Background())
+	ctx, cancel := context.WithCancel(ctx)
 	for _, s := range o.ss {
 		g.Go(func() error {
 			<-ctx.Done()
-			return (*s).Stop()
+			return s.Stop()
 		})
-		g.Go((*s).Run)
+		g.Go(s.Run)
 	}
-	for _, x := range o.extra {
-		g.Go(x)
-	}
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, o.signals...)
 	g.Go(func() error {
 		for {
 			select {
 			case <-ctx.Done():
 				return ctx.Err()
+			case <-ch:
+				if o.timeout != 0 {
+					g.Go(func() error {
+						time.After(o.timeout)
+						return fmt.Errorf("reach max quit wait time %v", o.timeout)
+					})
+				}
+				cancel()
+				return nil
 			}
 		}
 	})
 	return g.Wait()
-}
-
-func defaultSignalHandle() error {
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
-	return fmt.Errorf("receive signal %s", <-ch)
 }
